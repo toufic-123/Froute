9e00a6ed0951323769bb9694caf43013ee8094c0
Toufic <toufics99@gmail.com>
2024-01-07 00:56:04 -0700

    steps are extracted

--- location.py
+++ location.py
@@ -1,15 +1,37 @@
 import googlemaps
 import geocoder
 import random
+import urllib.request
+import json
 from pprint import pprint
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+
+import re
+
+
+
 API_KEY = "AIzaSyDqrIC47PIDREpzX_f-wdtY6ISPtqDPhA0"
 map_client = googlemaps.Client(API_KEY)
+directions_endpoint = "https://maps.googleapis.com/maps/api/directions/json?"
+geocode_endpoint = "https://maps.googleapis.com/maps/api/geocode/json?"
 
 
 def get_my_location():
     g = geocoder.ip('me')
     location = (g.latlng[0], g.latlng[1])
     return location
+
+def get_my_location_place_id(myLocation):
+    # PROBABY NOT NEEDED
+    gmap_location = map_client.reverse_geocode(
+        latlng=myLocation
+    )
+
+    #print(gmap_location.get('place_id'))
 
 def get_surrounding_locations(location, search_string,distance):
     response = map_client.places_nearby(
@@ -24,6 +46,37 @@ def get_random_location(business_list):
     rand = random.randint(0, len(business_list))
     return business_list[rand]
 
+def get_directions(origin, destination):
+    directions = map_client.directions(
+        origin=origin,
+        destination=f"place_id:{destination}",
+        mode='walking'
+    )
+    return directions
+
+def parse_json(json):
+    parsed_steps = []
+    print(json[0])
+    leg = json[0].get('legs')[0]
+    steps = leg.get('steps')
+    for step in steps:
+
+        # Define a regular expression pattern to match HTML tags
+        pattern = re.compile(r'<.*?>')
+
+        # Use the pattern to replace HTML tags with an empty string
+        cleaned_text = re.sub(pattern, '', step.get('html_instructions'))
+                      
+        parsed_steps.append(
+            {
+                "distance":step.get('distance'),
+                "html_instructions": cleaned_text,
+                "maneuver":step.get('maneuver')
+            }
+        )
+
+    return parsed_steps
+
 def main():
 
     location = get_my_location()
@@ -36,8 +89,18 @@ def main():
     business_list.extend(response.get('results'))
 
 
-    #pprint(business_list)
-    pprint(get_random_location(business_list))
+    rand_location = get_random_location(business_list)
 
+    # get_my_location_place_id(location) <- shitty bad code
+    directions = get_directions(location, rand_location.get('place_id'))
+
+    parsed_steps = parse_json(directions)
+
+    json_response = {"parsed_json": parsed_steps,
+                     "location_name":rand_location.get('name')}
+    
+    pprint(json_response)
+
+    # send json with steps and name of location
 
 main()
